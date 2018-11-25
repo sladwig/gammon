81e4b7789385ed4cc21238baad73a0de20df0935
Stefan Ladwig <koenig.s@gmail.com>
2018-11-25 15:54:26 +0200

    refactor(DiceArea): make DiceArea refer to this

--- src/DiceArea.js
+++ src/DiceArea.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import './DiceArea.css';
-import playerColor from './playerColor'
+import React from "react";
+import "./DiceArea.css";
+import playerColor from "./playerColor";
 
 class DiceArea extends React.Component {
   handleClick = () => {
-    const {currentPlayer, player, onClick} = this.props
-    if (currentPlayer === "any" || currentPlayer === player) {
-      onClick(player)
-    }
-  }
+    if (this.firstRound() || this.playersTurn())
+      this.props.onClick(this.props.player);
+  };
 
-  winnerArea = (winner) => {
+  winnerArea = winner => {
+    const { player } = this.props;
+    let winnerText = winner === player ? "We have a winner!" : "well...";
     return (
-      let winnerText = winner === player ? "We have a winner!" : "well..."
-      <div className={"dice-"+playerColor(player)} onClick={this.handleClick}>
+      <div className={"dice-" + playerColor(player)} onClick={this.handleClick}>
         <p>{winnerText}</p>
       </div>
     );
-  }
+  };
 
-  noRolled = (dices) => dices.length === 0
-  firsrRoundRoll = (dices) => Array.isArray(dices[0])
-  diceForPlayer = (dices) => dices.filter((dice) => (dice[0] === this.props.player)).map((dice) => (dice[1]))
-  playersTurn = () => this.props.currentPlayer === this.props.player
-  firstRound = () => this.props.currentPlayer === "any"
+  noRolled = dices => dices.length === 0;
+  firstRoundRoll = dices => Array.isArray(dices[0]);
+  diceForPlayer = dices =>
+    dices.filter(dice => dice[0] === this.props.player).map(dice => dice[1]);
+  playersTurn = () => this.props.currentPlayer === this.props.player;
+  firstRound = () => this.props.currentPlayer === "any";
   Dice = (dice, index) => (
-      <div className={'dice dice-'+dice} key={'dice-'+index}></div>
-    )
+    <div className={"dice dice-" + dice} key={"dice-" + index} />
+  );
 
   render() {
-    const {winner, player, currentPlayer, openDice} = this.props
+    const { winner, player, currentPlayer, openDice } = this.props;
 
     // winner text if we have a winner
-    if (winner) return winnerArea(winner)
+    if (winner) return this.winnerArea(winner);
 
-    let dices = openDice
+    let dices = openDice;
 
-    if (firstRoundRoll(dices)) dices = diceForPlayer(dices)
-    if (!playersTurn() && !firstRound()) dices = []
-    dices = dices.map(Dice)
-    if (noRolled(dices)) dices = <span>roll dice</span>
+    if (this.firstRoundRoll(dices)) dices = this.diceForPlayer(dices);
+    if (!this.playersTurn() && !this.firstRound()) dices = [];
+    dices = dices.map(this.Dice);
+    if (this.noRolled(dices)) dices = <span>roll dice</span>;
 
     return (
-      <div className={"dice-"+playerColor(player)} onClick={this.handleClick}>
+      <div className={"dice-" + playerColor(player)} onClick={this.handleClick}>
         {dices}
       </div>
     );
